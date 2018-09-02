3e57d5d69a8a8107c0858a56a308effdecf0fe2a
mr.kvinter <mr.kvinter@gmail.com>
2018-09-02 16:30:59 +0500

    Synchronizing main camera with virtual camera for pixel perfect

---
+++ Assets/Scripts/CameraSynchronizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraSynchronizer : MonoBehaviour
+{
+    private Camera camera;
+    public CinemachineVirtualCamera CinemachineVirtualCamera;
+    private float previousSize;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        UpdateSize();
+    }
+
+    private void Update()
+    {
+        if (Math.Abs(camera.orthographicSize - previousSize) > float.Epsilon) UpdateSize();
+    }
+
+    private void UpdateSize()
+    {
+        if (CinemachineVirtualCamera != null)
+        {
+            CinemachineVirtualCamera.m_Lens.OrthographicSize = camera.orthographicSize;
+            previousSize = camera.orthographicSize;
+        }
+    }
+}
