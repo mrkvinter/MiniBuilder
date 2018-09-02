de0e697b44d0d734bcc9e351850a6f457daccbc2
mr.kvinter <mr.kvinter@gmail.com>
2018-09-02 16:29:39 +0500

    MotionController for moving player

---
+++ Assets/Scripts/MotionController.cs
@@ -0,0 +1,33 @@
+﻿using Engine.DataContainers;
+using UnityEngine;
+
+public class MotionController : MonoBehaviour
+{
+    public float ForceJump;
+    public float Velocity;
+
+    private GroundedDataContainer groundedDataContainer;
+    private VelocityMotionDataContainer velocityMotionDataContainer;
+    private new Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        velocityMotionDataContainer = GetComponent<VelocityMotionDataContainer>();
+        groundedDataContainer = GetComponent<GroundedDataContainer>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        var horizontalVelocity = Input.GetAxis("Horizontal") * Velocity;
+        var verticalVelocity = rigidbody2D.velocity.y;
+
+        if (groundedDataContainer.Grounded && Input.GetButtonDown("Jump"))
+            verticalVelocity = ForceJump;
+
+        velocityMotionDataContainer.Velocity = new Vector2(horizontalVelocity, verticalVelocity);
+
+        rigidbody2D.velocity = velocityMotionDataContainer.Velocity;
+    }
+}
